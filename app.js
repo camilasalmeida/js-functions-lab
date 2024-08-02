880da6e98ecff3537023c0b0150a9bf302dff066
Camila Alm <camila.salmeidatech@hotmail.com>
2024-08-01 21:39:23 -0400

    exercise 9 pass

--- app.js
+++ app.js
@@ -160,7 +160,6 @@ Example: convertTemperature(32, 'C') should return 89.6 (Fahrenheit).
 Example: convertTemperature(32, 'F') should return 0 (Celsius).
 
 Complete the exercise in the space below:
-*/
 
 function convertTemperature(temperature, scale) {
     const celsiusScale = ['C', 'c', 'celsius', 'Celsius'];
@@ -176,5 +175,62 @@ function convertTemperature(temperature, scale) {
 }
 console.log('Exercise 8 Result:', convertTemperature(32, "C"));
 //exercise completed using the Declaration Function Expression.
+*/
+
+/*
+Exercise 9: basicCalculator()
+
+Create a function named basicCalculator. 
+It should take three arguments: two numbers and a string representing 
+an operation ('add', 'subtract', 'multiply', 'divide'). 
+Perform the provided operation on the two numbers. 
+In operations where the order of numbers is important, 
+treat the first parameter as the first operand and the 
+second parameter as the second operand.
+
+Example: basicCalculator(10, 5, 'subtract') should return 5.
+
+Complete the exercise in the space below:
+*/
+/*
+function basicCalculator(num1, num2, operador) {
+        if (operador === 'divide') {
+            return num1 / num2;
+        } else if (operador === 'multiply') {
+                return num1 * num2;
+        } else if (operador === 'add') {
+                return num1 + num2;
+        } else if (operador === 'subtract') {
+                return num1 - num2;
+        }
+}
+
+console.log('Exercise 9 Result:', basicCalculator(10, 5, "subtract"));
+*/
+
+/*
+//or using the switch method:
+function basicCalculator(num1, num2, operador) {
+    switch (operador) {
+        case 'add':
+            return num1 + num2;
+            break;
+        case 'multiply':
+            return num1 * num2;
+            break;
+        case 'divide':
+            return num1 / num2;
+            break;
+        case 'subtract':
+            return num1 - num2;
+            break;
+        default: 
+            console.log('Invalid input');
+    }
+}
+
+console.log('Exercise 9 Result:', basicCalculator(10, 5, "subtract"));
+*/
+
 
 
